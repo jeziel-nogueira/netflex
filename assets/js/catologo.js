32d0aa9811e0e4e00280362baa848912d9c0a190
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2023-11-15 20:48:15 -0300

    update catalogo

--- assets/js/catologo.js
+++ assets/js/catologo.js
@@ -1,6 +1,6 @@
 let userLogado = JSON.parse(localStorage.getItem('userLogado'));
 let poster = document.querySelector('.movie');
-let main = document.getElementById('main');
+let destaque = document.getElementById('destaque');
 let top_rated = document.getElementById('top_rated');
 let lancamentos = document.getElementById('lancamentos');
 //let top_rated = document.getElementById('top_rated');
@@ -17,112 +17,135 @@ function sair(){
 
 let movies = [];
 
-const API_KEY = 'api_key=3b13d1c5fa5a243ae3eb4ce36a871a8a';
+const API_KEY = '';
 
 const IMG_URL = 'https://image.tmdb.org/t/p/w500/';
-const BASE_URL = 'https://api.themoviedb.org/3/';
-const LANGUAGE = '&language=pt-br'
-const API_URL = BASE_URL + 'discover/tv?sort_by=popularity.desc&' + API_KEY + LANGUAGE;
+const BASE_URL = 'https://api.themoviedb.org/3/discover/movie?api_key=3b13d1c5fa5a243ae3eb4ce36a871a8a';
+const LANGUAGE = '&language=pt-BR';
+
+const TOP_POPULARITY = '&sort_by=popularity.desc';
+const TOP_RATED = '&sort_by=vote_average.desc';
+
+const BR_MOVIES = BASE_URL + TOP_POPULARITY + '&region=BR' + LANGUAGE;
+const FUTUROS =  BASE_URL + '&language=pt-BR&page=1&include_adult=true&year=2024&query=mm';
+const API_URL = BASE_URL + TOP_RATED  + LANGUAGE + '&region=BR';
 
 
-getMovies(API_URL);
 
-function getMovies(url){
-    fetch(url)
+destaques();
+lancamentosFuturos();
+topRated();
+
+
+// busca os filmes em destaque para exibir no catalofo
+function destaques(){
+
+    fetch(BR_MOVIES)
+            .then(respose => respose.json())
+            .then(data =>{
+                movies = data.results.slice(0,6)
+                
+                destaque.innerHTML = '';
+                movies.forEach(movie => {
+
+                    const {title, poster_path, vote_average, overview} = movie
+                    const movieElement = document.createElement('div');
+
+                    movieElement.classList.add('movie');
+                    movieElement.innerHTML = `
+                        <div class="movie">
+                            <img class="capa" src="${IMG_URL+poster_path}" alt="${title}">
+                                <div class="movieinfo">
+                                    <h3>${title}</h3>
+                                    <span class="green">
+                                        <img class="star" src="../img/Star.svg">
+                                        ${vote_average}
+                                    </span>                
+                                </div>
+                            <div class="overview">
+                                ${overview}
+                            </div>
+                        </div>  
+                    `
+                    destaque.appendChild(movieElement);
+                })
+            }
+    );
+    
+}
+
+function lancamentosFuturos(){
+
+    
+        fetch(FUTUROS)
+            .then(respose => respose.json())
+            .then(data =>{
+                movies = data.results.slice(0,6)
+                
+                
+                lancamentos.innerHTML = '';
+                movies.forEach(movie => {
+
+                    const {title, poster_path, vote_average, overview} = movie
+                    const movieElement = document.createElement('div');
+
+                    movieElement.classList.add('movie');
+                    movieElement.innerHTML = `
+                        <div class="movie">
+                            <img class="capa" src="${IMG_URL+poster_path}" alt="${title}">
+                                <div class="movieinfo">
+                                    <h3>${title}</h3>
+                                    <span class="green">
+                                        <img class="star" src="../img/Star.svg">
+                                        ${vote_average}
+                                    </span>                
+                                </div>
+                            <div class="overview">
+                                ${overview}
+                            </div>
+                        </div>  
+                    `
+                    lancamentos.appendChild(movieElement);
+                })
+            }
+        );
+
+}
+
+function topRated(){
+
+    fetch(API_URL)
         .then(respose => respose.json())
         .then(data =>{
             movies = data.results.slice(0,6)
-            //Movie();
-            topMovie();
-            Lancamentos();
+
+            console.log(movies);
+            top_rated.innerHTML = '';
+            movies.forEach(movie => {
+
+                const {title, poster_path, vote_average, overview} = movie
+                const movieElement = document.createElement('div');
+
+                movieElement.classList.add('movie');
+                movieElement.innerHTML = `
+                    <div class="movie">
+                        <img class="capa" src="${IMG_URL+poster_path}" alt="${title}">
+                            <div class="movieinfo">
+                                <h3>${title}</h3>
+                                <span class="green">
+                                    <img class="star" src="../img/Star.svg">
+                                    ${vote_average}
+                                </span>                
+                            </div>
+                        <div class="overview">
+                            ${overview}
+                        </div>
+                    </div>  
+                `
+                top_rated.appendChild(movieElement);
+            })
         }
     );
-}
 
-
-
-function Movie(){
-    main.innerHTML = '';
-    movies.forEach(movie => {
-        console.log(movie.id)
-        console.log(movie.original_name)
-
-        const ratingImg = document.createElement('img');
-        const {original_name, poster_path, vote_average, overview} = movie
-        const movieElement = document.createElement('div');
-
-        movieElement.classList.add('movie');
-        movieElement.innerHTML = `
-            <div class="movie">
-                <img class="capa" src="${IMG_URL+poster_path}" alt="${original_name}">
-                    <div class="movieinfo">
-                        <h3>${original_name}</h3>
-                        <span class="green">
-                            <img class="star" src="../img/Star.svg">
-                            ${vote_average}
-                        </span>                
-                    </div>
-                <div class="overview">
-                    ${overview}
-                </div>
-            </div>  
-        `
-        main.appendChild(movieElement);
-    })
     
-};
-
-function topMovie(){
-    top_rated.innerHTML = '';
-    movies.forEach(movie => {
-
-        const {original_name, poster_path, vote_average, overview} = movie
-        const movieElement = document.createElement('div');
-
-        movieElement.classList.add('movie');
-        movieElement.innerHTML = `
-            <div class="movie">
-                <img class="capa" src="${IMG_URL+poster_path}" alt="${original_name}">
-                    <div class="movieinfo">
-                        <h3>${original_name}</h3>
-                        <span class="green">
-                            <img class="star" src="../img/Star.svg">
-                            ${vote_average}
-                        </span>                
-                    </div>
-                <div class="overview">
-                    ${overview}
-                </div>
-            </div>  
-        `
-        top_rated.appendChild(movieElement);
-    })
-    
-};
-
-function Lancamentos(){
-    lancamentos.innerHTML = '';
-    movies.forEach(movie => {
-
-        const {original_name, poster_path, vote_average, overview} = movie
-        const movieElement = document.createElement('div');
-
-        movieElement.classList.add('movie');
-        movieElement.innerHTML = `
-            <div class="movie">
-                <img class="capa" src="${IMG_URL+poster_path}" alt="${original_name}">
-                    <div class="movieinfo">
-                        <h3>${original_name}</h3>
-                        <span class="green">
-                            <img class="star" src="../img/Star.svg">
-                            ${vote_average}
-                        </span>                
-                    </div>
-                <div class="overview">
-                    ${overview}
-                </div>
-            </div>  
-        `
-        lancamentos.appendChild(movieElement);
-    })
 }
