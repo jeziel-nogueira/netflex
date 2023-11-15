ce3376b68bcc8d684cd725e96ad16174d6452736
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2023-11-15 03:44:01 -0300

    TMDB api_key ok

--- assets/js/catologo.js
+++ assets/js/catologo.js
@@ -1,6 +1,9 @@
 let userLogado = JSON.parse(localStorage.getItem('userLogado'));
 let poster = document.querySelector('.movie');
 let main = document.getElementById('main');
+let top_rated = document.getElementById('top_rated');
+let lancamentos = document.getElementById('lancamentos');
+//let top_rated = document.getElementById('top_rated');
 
 if(localStorage.getItem('token') == null){
     window.location.href= '../../index.html'
@@ -21,28 +24,43 @@ const BASE_URL = 'https://api.themoviedb.org/3/';
 const LANGUAGE = '&language=pt-br'
 const API_URL = BASE_URL + 'discover/tv?sort_by=popularity.desc&' + API_KEY + LANGUAGE;
 
-fetch(API_URL)
-    .then(respose => respose.json())
-    .then(data =>{
-        movies = data.results.slice(0,12)
-    })
+
+getMovies(API_URL);
+
+function getMovies(url){
+    fetch(url)
+        .then(respose => respose.json())
+        .then(data =>{
+            movies = data.results.slice(0,6)
+            //Movie();
+            topMovie();
+            Lancamentos();
+        }
+    );
+}
+
+
 
 function Movie(){
-
     main.innerHTML = '';
     movies.forEach(movie => {
         console.log(movie.id)
         console.log(movie.original_name)
 
+        const ratingImg = document.createElement('img');
         const {original_name, poster_path, vote_average, overview} = movie
         const movieElement = document.createElement('div');
+
         movieElement.classList.add('movie');
         movieElement.innerHTML = `
             <div class="movie">
                 <img class="capa" src="${IMG_URL+poster_path}" alt="${original_name}">
                     <div class="movieinfo">
                         <h3>${original_name}</h3>
-                        <span class="green">${vote_average}</span>                
+                        <span class="green">
+                            <img class="star" src="../img/Star.svg">
+                            ${vote_average}
+                        </span>                
                     </div>
                 <div class="overview">
                     ${overview}
@@ -50,5 +68,61 @@ function Movie(){
             </div>  
         `
         main.appendChild(movieElement);
+    })
+    
+};
+
+function topMovie(){
+    top_rated.innerHTML = '';
+    movies.forEach(movie => {
+
+        const {original_name, poster_path, vote_average, overview} = movie
+        const movieElement = document.createElement('div');
+
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
+            <div class="movie">
+                <img class="capa" src="${IMG_URL+poster_path}" alt="${original_name}">
+                    <div class="movieinfo">
+                        <h3>${original_name}</h3>
+                        <span class="green">
+                            <img class="star" src="../img/Star.svg">
+                            ${vote_average}
+                        </span>                
+                    </div>
+                <div class="overview">
+                    ${overview}
+                </div>
+            </div>  
+        `
+        top_rated.appendChild(movieElement);
+    })
+    
+};
+
+function Lancamentos(){
+    lancamentos.innerHTML = '';
+    movies.forEach(movie => {
+
+        const {original_name, poster_path, vote_average, overview} = movie
+        const movieElement = document.createElement('div');
+
+        movieElement.classList.add('movie');
+        movieElement.innerHTML = `
+            <div class="movie">
+                <img class="capa" src="${IMG_URL+poster_path}" alt="${original_name}">
+                    <div class="movieinfo">
+                        <h3>${original_name}</h3>
+                        <span class="green">
+                            <img class="star" src="../img/Star.svg">
+                            ${vote_average}
+                        </span>                
+                    </div>
+                <div class="overview">
+                    ${overview}
+                </div>
+            </div>  
+        `
+        lancamentos.appendChild(movieElement);
     })
 }
