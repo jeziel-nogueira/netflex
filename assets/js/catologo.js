9b7a92c41fbf86bf6c1078373247b4159e8c223c
jeziel nogueira <jezielnogueira_15@yahoo.com.br>
2023-11-27 01:52:37 -0300

    corrigindo filtros api

--- assets/js/catologo.js
+++ assets/js/catologo.js
@@ -26,6 +26,10 @@ const BASE_URL = 'https://api.themoviedb.org/3/';
 const LANGUAGE = '&language=pt-br'
 const API_URL = BASE_URL + 'discover/tv?sort_by=popularity.desc&' + API_KEY + LANGUAGE;
 
+
+const FUTUROS =  'https://api.themoviedb.org/3/discover/movie?' + API_KEY + '&with_genres=28';
+const POPULARES = 'https://api.themoviedb.org/3/movie/popular?api_key=3b13d1c5fa5a243ae3eb4ce36a871a8a';
+
 let lista = [];
 
 getMovies(API_URL);
@@ -36,7 +40,7 @@ function getMovies(url){
         .then(data =>{
             movies = data.results.slice(0,6)
             topMovie();
-            Lancamentos();
+            //Lancamentos();
         }
     );
 }
@@ -73,33 +77,44 @@ function topMovie(){
     
 };
 
+lancamentosFuturos();
 
-function Lancamentos(){
-    lancamentos.innerHTML = '';
-    movies.forEach(movie => {
+function lancamentosFuturos(){
 
-        const {original_name, poster_path, vote_average, overview, id} = movie
-        const movieElement = document.createElement('div');
+    
+    fetch(POPULARES)
+        .then(respose => respose.json())
+        .then(data =>{
+            movies = data.results.slice(0,6)
+            
+            
+            lancamentos.innerHTML = '';
+            movies.forEach(movie => {
+                lista.push(movie);
+                const {id, title, poster_path, vote_average, overview} = movie
+                const movieElement = document.createElement('div');
 
-        lista.push(movie);
-        movieElement.classList.add('movie');
-        movieElement.innerHTML = `
-            <div class="movie" onClick="loadMovie(${id})">
-                <img class="capa" src="${IMG_URL+poster_path}" alt="${original_name}">
-                    <div class="movieinfo">
-                        <h3>${original_name}</h3>
-                        <span class="green">
-                            <img class="star" src="../img/Star.svg">
-                            ${vote_average}
-                        </span>                
-                    </div>
-                <div class="overview">
-                    ${overview}
-                </div>
-            </div>  
-        `
-        lancamentos.appendChild(movieElement);
-    })
+                movieElement.classList.add('movie');
+                movieElement.innerHTML = `
+                    <div class="movie">
+                        <img class="capa" src="${IMG_URL+poster_path}" alt="${title}"  onClick="loadMovie(${id})">
+                            <div class="movieinfo">
+                                <h3>${title}</h3>
+                                <span class="green">
+                                    <img class="star" src="../img/Star.svg">
+                                    ${vote_average}
+                                </span>                
+                            </div>
+                        <div class="overview">
+                            ${overview}
+                        </div>
+                    </div>  
+                `
+                lancamentos.appendChild(movieElement);
+            })
+        }
+    );
+
 }
 
 function loadMovie(val){
